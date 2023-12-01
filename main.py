5526b02b2ed6d9e9294cf1eb7e8a759e83726159
armaangupta06 <68565234+armaangupta06@users.noreply.github.com>
2023-11-30 19:13:03 -0500

    Add testcase to main.py for S-Curve.

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import time
 
 
 def main():
-    path = path_with_points( Point(0, 0), Point(36, 36), initial_heading=0, final_heading=0,  tangent_magnitude=1.5)
-    steps = calculate_trajectory(path, 50, 2, 25)
+    start = time.time()
+    path = path_with_points( Point(0, 0), Point(36, 36), Point(0, 36), Point(-100, 100), initial_heading=0, final_heading=0,  tangent_magnitude=1)
+    steps = calculate_trajectory(path, 50, 2, 25, max_j=20, trap=False)
 
+    end = time.time()
+    print(end-start)
+
+    print(len(steps))
     graph_path(steps)
 main()
